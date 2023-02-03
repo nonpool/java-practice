8d158d5132ee3793bc8e0410bf9de5e061407669
LorettaYoo <luoyutong598@163.com>
2023-02-03 09:21:22 +0800

    feat: use reflection convert class to json

--- reflection_1/src/main/java/com/nonpool/ObjectToJsonApplication.java
+++ reflection_1/src/main/java/com/nonpool/ObjectToJsonApplication.java
@@ -1,10 +1,29 @@
 package com.nonpool;
 
+import java.lang.reflect.Field;
+
 public class ObjectToJsonApplication {
 
     public String convert(Object any) {
         // todo
-        return "";
+        Class<?> anyClass = any.getClass();
+        Field[] fields = anyClass.getDeclaredFields();
+
+        StringBuilder stringBuilder = new StringBuilder("{");
+        for (Field field : fields) {
+            String name = field.getName();
+            field.setAccessible(true);
+            try {
+                Object value = field.get(any);
+                stringBuilder.append(',').append('"').append(name).append('"').append(":").append('"').append(value).append('"');
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+        stringBuilder.append("}");
+
+        return String.valueOf(stringBuilder).replaceFirst(",","");
     }
 
 }
