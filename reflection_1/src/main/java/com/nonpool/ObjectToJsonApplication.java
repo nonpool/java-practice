16bba5ae6fd3170745f20b9ddb4c3e336429ea0a
LorettaYoo <luoyutong598@163.com>
2023-02-07 10:55:12 +0800

    feat: get fields by getDeclaredMethods but can't handle the order of fields

--- reflection_1/src/main/java/com/nonpool/ObjectToJsonApplication.java
+++ reflection_1/src/main/java/com/nonpool/ObjectToJsonApplication.java
@@ -1,29 +1,48 @@
 package com.nonpool;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ObjectToJsonApplication {
 
-    public String convert(Object any) {
+    public String convert(Object any) throws RuntimeException {
         // todo
         Class<?> anyClass = any.getClass();
-        Field[] fields = anyClass.getDeclaredFields();
+        boolean includeNullField = isIncludeNullField(anyClass);
+        Method[] declaredMethods = anyClass.getDeclaredMethods();
 
         StringBuilder stringBuilder = new StringBuilder("{");
-        for (Field field : fields) {
-            String name = field.getName();
-            field.setAccessible(true);
-            try {
-                Object value = field.get(any);
-                stringBuilder.append(',').append('"').append(name).append('"').append(":").append('"').append(value).append('"');
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
+        for (Method method : declaredMethods) {
+            String methodName = method.getName();
+            String fieldName = methodName.substring(3);
+            String s1 = fieldName.substring(0, 1).toLowerCase();
+            String s2 = fieldName.substring(1);
+            fieldName = s1 + s2;
+            if (methodName.startsWith("get")) {
+                try {
+                    Object field = method.invoke(any);
+                    if (includeNullField  || field != null ) {
+                        String fieldValue = field == null ? null : ("\"" + field + "\"");
+                        stringBuilder.append(",\"").append(fieldName).append("\":").append(fieldValue);
+                    }
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
             }
-
         }
         stringBuilder.append("}");
-
-        return String.valueOf(stringBuilder).replaceFirst(",","");
+        return String.valueOf(stringBuilder).replaceFirst(",", "");
     }
+
+    private static boolean isIncludeNullField(Class<?> anyClass) {
+        boolean annotationPresent = anyClass.isAnnotationPresent(ToJsonConfig.class);
+        boolean includeNullField = true;
+        if (annotationPresent) {
+            ToJsonConfig toJsonConfig = anyClass.getAnnotation(ToJsonConfig.class);
+            includeNullField = toJsonConfig.includeNullField();
+        }
+        return includeNullField;
+    }
+
 
 }
